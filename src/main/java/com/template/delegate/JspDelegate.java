05b2765eed4970a62a3cc4ce4fd7ba0154bd8ce7
Vince Via <vince@viaoa.com>
2023-10-22 09:23:29 -0400

    jsp delegate

--- src/main/java/com/template/delegate/JspDelegate.java
+++ src/main/java/com/template/delegate/JspDelegate.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 import com.template.model.oa.*;
 import com.template.jsp.oa.*;
 import com.viaoa.hub.*;
-import com.viaoa.jsp.*;
 import com.viaoa.util.*;
+import com.viaoa.web.server.OASession;
 
 public class JspDelegate {
 	private static Logger LOG = Logger.getLogger(JspDelegate.class.getName());
